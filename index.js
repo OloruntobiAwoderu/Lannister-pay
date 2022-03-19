b0f1287673b632b1191f3e4fc56e3d5b901afd4d
Oloruntobi Awoderu <awoderuoloruntobi@gmail.com>
2022-03-19 10:19:38 +0100

    Chore: Added home route

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 import express, { json } from "express";
 import routes from "./routes";
 import { redisClient } from "./db";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.use(json());
 app.use(routes);
+app.use("/", (req, res) => {
+	return res.status(200).json("Hello World");
+});
 
 app.use((req, res, next) => next({ status: 404, customMessage: "Not Found" }));
 
 app.use((err, req, res, next) => {
-  res
-    .status(err.status || 500)
-    .json({
-      Error:
-        err.customMessage ||
-        "Error while processing request. It's not you, it's us",
-    });
+	res.status(err.status || 500).json({
+		Error:
+			err.customMessage ||
+			"Error while processing request. It's not you, it's us",
+	});
 });
 
-const port = process.env.PORT 
+const port = process.env.PORT;
 redisClient.connect();
 
 app.listen(port, () => {
-  console.log(`RUNNING on ${port}`);
+	console.log(`RUNNING on ${port}`);
 });
